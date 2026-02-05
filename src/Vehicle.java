c4367cc36340480c8be5466cc2fe5eb57726f581
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2026-02-05 22:05:56 +0530

    Created abstract class 'Vehicle' with private attributes

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public abstract class Vehicle {
+    private String vehicleId;
+    private String brand;
+    private String model;
+    private double baseRatePerDay;
+    private boolean isAvailable;
+
+
+}
