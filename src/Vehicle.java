66b89fbb2585fda283031df00abb772a8976fb6e
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2026-02-05 22:07:14 +0530

    Created parameterized constructor to initialize all private variables

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -5,5 +5,17 @@ public abstract class Vehicle {
     private double baseRatePerDay;
     private boolean isAvailable;
 
-
+    public Vehicle(
+            String vehicleId,
+            String brand,
+            String model,
+            double baseRatePerDay,
+            boolean isAvailable
+    ){
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.model = model;
+        this.baseRatePerDay = baseRatePerDay;
+        this.isAvailable = isAvailable;
+    }
 }
