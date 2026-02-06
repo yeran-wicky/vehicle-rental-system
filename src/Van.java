68bcb62d8651626a50b5ec2d03f6b6f0e2a3411d
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2026-02-06 14:14:54 +0530

    Created 'Van.java' subclass and added all required attributes and methods

---
+++ src/Van.java
@@ -0,0 +1,25 @@
+public class Van extends Vehicle{
+
+    private double cargoCapacityKg;
+
+    public Van(
+            String vehicleId,
+            String brand,
+            String model,
+            double baseRatePerDay,
+            boolean isAvailable,
+            double cargoCapacityKg
+    ){
+        super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        this.cargoCapacityKg = cargoCapacityKg;
+    }
+
+    public double getCargoCapacityKg() {
+        return cargoCapacityKg;
+    }
+
+    @Override
+    public double calculateRentalCost(int days) {
+        return getBaseRatePerDay() * days + (getCargoCapacityKg() * 0.2 * days);
+    }
+}
