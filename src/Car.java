35788255134557408d4934bbaa928a103b6627a1
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2026-02-06 08:43:13 +0530

    Created subclass 'Car' and initialized all the attributes needed

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car extends Vehicle{
+
+    private int numberOfSeats;
+
+    public Car(
+            String vehicleId,
+            String brand,
+            String model,
+            double baseRatePerDay,
+            boolean isAvailable,
+            int numberOfSeats
+    ){
+        super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        this.numberOfSeats = numberOfSeats;
+    }
+}
