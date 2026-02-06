f3cef32224cab742f90e17cc9a1692ac5197cb92
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2026-02-06 19:56:46 +0530

    Created main method in 'RentalApp.java' class and designed main menu

--- src/RentalApp.java
+++ src/RentalApp.java
@@ -4,4 +4,18 @@ public class RentalApp {
 
     public static ArrayList <Vehicle> vArrList = new ArrayList <Vehicle> ();
     public static Scanner sc = new Scanner (System.in);
+
+    public static void main(String[] args){
+        while(true) {
+            System.out.println("\n===== Vehicle Rental Management System =====");
+            System.out.println("1. Add a vehicle");
+            System.out.println("2. View all vehicles");
+            System.out.println("3. Rent a vehicle");
+            System.out.println("4. Return a vehicle");
+            System.out.println("5. Search vehicle by ID");
+            System.out.println("6. View total rental income");
+            System.out.println("7. Exit");
+            System.out.println("\nChoose an option");
+        }
+    }
 }
