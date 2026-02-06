b7a230ea9bf15d75513a0388173dce31315e2dfe
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2026-02-06 15:32:33 +0530

    Created class 'RentalApp' containing the main method and added static Vehicle arraylist and Scanner

---
+++ src/RentalApp.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class RentalApp {
+
+    public static ArrayList <Vehicle> vArrList = new ArrayList <Vehicle> ();
+    public static Scanner sc = new Scanner (System.in);
+}
