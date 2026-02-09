266611c5110e80a3be9a3bbf99cb467e7c8bbf23
ZaraDil <cit-25-01-0116@sltc.edu.lk>
2026-02-09 23:54:09 +0530

    update car comment

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,8 @@
 public class Car extends Vehicle {
+    //Car(child) inherits all features from Vehicles(parent).
 
     private int numberOfSeats;
+    //Car has a unique attribute which is engineCapacityCC
 
     public Car(
             String vehicleId,
@@ -9,17 +11,23 @@ public class Car extends Vehicle {
             double baseRatePerDay,
             boolean isAvailable,
             int numberOfSeats
+            //create new car object
     ){
         super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        /*common data (ID, Brand, Model, etc.) up to the parent Vehicle class so the parent can handle it.
+        The Car class doesn't need to save these manually because Vehicle does it.*/
         this.numberOfSeats = numberOfSeats;
+        //numberOfSeats you gave when creating the car and saves it into the private variable inside this class.
     }
 
     public int getNumberOfSeats() {
         return numberOfSeats;
     }
+    //getter
 
     @Override
     public double calculateRentalCost(int days) {
         return getBaseRatePerDay() * days + (getNumberOfSeats() * 200 * days);
+        //Calculate the cost 
     }
 }
