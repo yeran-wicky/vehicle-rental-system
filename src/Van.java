49f0a16510ea5ec90653fe91d2448023c6a9f5bf
chayachathumi <chayachathumi@gmail.com>
2026-02-10 00:24:40 +0530

    Create comments for Van.java

--- src/Van.java
+++ src/Van.java
@@ -2,6 +2,7 @@ public class Van extends Vehicle{
 
     private double cargoCapacityKg;
 
+    // Constructors
     public Van(
             String vehicleId,
             String brand,
@@ -10,14 +11,16 @@ public class Van extends Vehicle{
             boolean isAvailable,
             double cargoCapacityKg
     ){
-        super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        super(vehicleId, brand, model, baseRatePerDay, isAvailable);// Parent Constructors
         this.cargoCapacityKg = cargoCapacityKg;
     }
 
+    // Getter Method
     public double getCargoCapacityKg() {
         return cargoCapacityKg;
     }
 
+    // Use abstract method to calculate van rental cost
     @Override
     public double calculateRentalCost(int days) {
         return getBaseRatePerDay() * days + (getCargoCapacityKg() * 0.2 * days);
