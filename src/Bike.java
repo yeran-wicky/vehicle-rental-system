3137dde3cf9a84a4b6d9a2538f2f062b26ac976b
ZaraDil <cit-25-01-0116@sltc.edu.lk>
2026-02-09 23:37:28 +0530

    BIKE COMMENT

--- src/Bike.java
+++ src/Bike.java
@@ -23,3 +23,16 @@ public class Bike extends Vehicle{
         return getBaseRatePerDay() * days + (getEngineCapacityCC() * 0.5 * days);
     }
 }
+
+/*Bike(child) inherits all features from Vehicles(parent).Also bike has a unique attribute which is engineCapacityCC
+,from VehicleID,brand, model,baseRatePerDay,availability,engineCapacityCC can create new bike object .
+It sends the common data (ID, Brand, Model, etc.) up to the parent Vehicle class so the parent can handle it.
+The Bike class doesn't need to save these manually because Vehicle does it.It is passed when you create
+object but as private variable inside the class.From getter can get "What is the engine size of this bike?"
+The parent class already has calculateRentalCost method but  need to replace special method for bikes.
+It calculates the price differently than a normal Vehicle.When  calculate the bike bill,
+it charges extra for bigger engines!
+
+
+ */
+
