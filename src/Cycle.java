05a91c0f3694e3fc69cf93d5fdd8987bdf01289e
ZaraDil <cit-25-01-0116@sltc.edu.lk>
2026-02-10 00:02:54 +0530

    update cycle comment

--- src/Cycle.java
+++ src/Cycle.java
@@ -11,17 +11,23 @@ public class Cycle extends Vehicle{
             double baseRatePerDay,
             boolean isAvailable,
             int gearCount
+            //create new cycle object
     ){
         super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        /*common data (ID, Brand, Model, etc.) up to the parent Vehicle class so the parent can handle it.
+        The cycle class doesn't need to save these manually because Vehicle does it.*/
         this.gearCount = gearCount;
+        //gear count you gave when creating the cycle and saves it into the private variable inside this class.
     }
 
     public int getGearCount() {
         return gearCount;
     }
+    //getter
 
     @Override
     public double calculateRentalCost(int days) {
         return (getBaseRatePerDay() * days) + (getGearCount() * 150);
     }
+    //calculate the cost
 }
