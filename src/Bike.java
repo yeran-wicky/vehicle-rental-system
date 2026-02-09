c3df45ebe279a8def4b132b4808a856e419b4758
ZaraDil <cit-25-01-0116@sltc.edu.lk>
2026-02-09 23:49:30 +0530

    2nd update bike comment

--- src/Bike.java
+++ src/Bike.java
@@ -1,6 +1,9 @@
 public class Bike extends Vehicle{
+    //Bike(child) inherits all features from Vehicles(parent).
 
     private int engineCapacityCC;
+    //bike has a unique attribute which is engineCapacityCC
+
 
     public Bike(
             String vehicleId,
@@ -9,30 +12,28 @@ public class Bike extends Vehicle{
             double baseRatePerDay,
             boolean isAvailable,
             int engineCapacityCC
+            //create new bike object
     ){
         super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        /*common data (ID, Brand, Model, etc.) up to the parent Vehicle class so the parent can handle it.
+        The Bike class doesn't need to save these manually because Vehicle does it.*/
         this.engineCapacityCC = engineCapacityCC;
+        //engineCapacityCC you gave when creating the bike and saves it into the private variable inside this class.
+
+
     }
 
     public int getEngineCapacityCC() {
         return engineCapacityCC;
+        //getter
     }
 
     @Override
     public double calculateRentalCost(int days) {
         return getBaseRatePerDay() * days + (getEngineCapacityCC() * 0.5 * days);
+        //Calculat the Cost
     }
 }
 
-/*Bike(child) inherits all features from Vehicles(parent).Also bike has a unique attribute which is engineCapacityCC
-,from VehicleID,brand, model,baseRatePerDay,availability,engineCapacityCC can create new bike object .
-It sends the common data (ID, Brand, Model, etc.) up to the parent Vehicle class so the parent can handle it.
-The Bike class doesn't need to save these manually because Vehicle does it.It is passed when you create
-object but as private variable inside the class.From getter can get "What is the engine size of this bike?"
-The parent class already has calculateRentalCost method but  need to replace special method for bikes.
-It calculates the price differently than a normal Vehicle.When  calculate the bike bill,
-it charges extra for bigger engines!
 
-
- */
 
